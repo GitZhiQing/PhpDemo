0c0c29fad2a91b2755c43ab12d2b93072ee78a79
QING <qlear@qq.com>
2024-12-15 23:54:48 +0800

    docs: 完善 README

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 $host = 'localhost';
-$port = '3306';
-$db_name = 'demo';
-$db_username = 'admin';
-$db_password = '123456';
+$port = '3306';     // MySQL 默认端口为 3306
+$db_name = '';      // 数据库名
+$db_username = '';  // 数据库用户名
+$db_password = '';  // 数据库密码
 
 $conn = new mysqli($host, $db_username, $db_password, $db_name, $port);
 
